309435c6e7e0434ca2418cf9c17d7b61c0052aba
snehalkurle21-dot <snehalkurle21@gmail.com>
2026-01-15 12:37:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
 //your JS code here. If required.
+const codes = document.querySelectorAll('.code');
+
+// Focus first input on page load
+codes[0].focus();
+
+codes.forEach((code, index) => {
+    code.addEventListener('keydown', (e) => {
+
+        // Move forward on number input
+        if (e.key >= '0' && e.key <= '9') {
+            code.value = '';
+            setTimeout(() => {
+                if (codes[index + 1]) {
+                    codes[index + 1].focus();
+                }
+            }, 10);
+        }
+
+        // Move backward on backspace
+        if (e.key === 'Backspace') {
+            if (code.value === '' && codes[index - 1]) {
+                codes[index - 1].focus();
+                codes[index - 1].value = '';
+            }
+        }
+    });
+});
